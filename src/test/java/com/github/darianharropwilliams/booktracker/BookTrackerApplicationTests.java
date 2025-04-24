77405717a13add9cacd5372ef8776da7a503f808
DarianHarropWilliams <darianharropwilliams@gmail.com>
2025-04-24 12:38:02 -0400

    Initial commit: Spring Boot setup

---
+++ src/test/java/com/github/darianharropwilliams/booktracker/BookTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.darianharropwilliams.booktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
